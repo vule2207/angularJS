7aa4f5ef8f429e5f8cbda42145078172b37a3f77
Le Thanh Vu <levu2207@hanbiro.vn>
2023-04-24 14:43:48 +0700

    update

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
 var userManagement = angular.module("userManagement", []);
 userManagement.controller("userController", function ($scope, $http) {
 	let limit = 10;
-	// handle CRUD
 	$scope.isAdd = true;
+	$scope.formData = {};
+	$scope.curPage = 1;
+
+	// pagination
+	$scope.handlePageChange = function (page) {
+		if (page <= 0) {
+			$scope.curPage = 1;
+			return;
+		}
+		if (page > $scope.totalPages) {
+			$scope.curPage = $scope.totalPages;
+			return;
+		}
+		$scope.curPage = page;
+		const skip = ($scope.curPage - 1) * limit;
+		$scope.getUsers(limit, skip);
+	};
+	// handle CRUD
 	$scope.handleEditUser = function (user) {
 		$scope.isAdd = false;
-		$scope.firstName = user.firstName;
-		$scope.lastName = user.lastName;
-		$scope.gender = user.gender;
-		$scope.email = user.email;
-		$scope.phone = user.phone;
-		$scope.age = user.age;
+		$scope.formData.id = user.id;
+		$scope.formData.firstName = user.firstName;
+		$scope.formData.lastName = user.lastName;
+		$scope.formData.gender = user.gender;
+		$scope.formData.email = user.email;
+		$scope.formData.phone = user.phone;
+		$scope.formData.age = user.age;
 	};
 
 	// CRUD users data
-	$scope.getUsers = function () {
-		$http
-			.get(`https://dummyjson.com/users${limit !== 0 ? `?limit=${limit}` : ""}`)
+	$scope.getUsers = function (limit = 10, skip = 0) {
+		const config = {
+			method: "GET",
+			url: "https://dummyjson.com/users",
+			params: {
+				limit: limit,
+				skip: skip,
+			},
+		};
+		$http(config) //${limit !== 0 ? `?limit=${limit}` : ""}
 			.then(function (response) {
 				$scope.users = response.data.users;
+				$scope.total = response.data.total;
+				$scope.totalPages = Math.ceil($scope.total / limit);
 			})
 			.catch(function (error) {
 				console.log(error);
@@ -37,14 +64,40 @@ userManagement.controller("userController", function ($scope, $http) {
 			});
 	};
 
-	$scope.handleSubmit = function () {
-		alert("hello world");
+	$scope.handleSubmit = function (form) {
+		// console.log("form:", form);
+		$scope.userData = angular.copy($scope.formData);
+		if ($scope.isAdd) {
+			$http
+				.post(`https://dummyjson.com/users/add`, $scope.userData)
+				.then((res) => {
+					$scope.formData = {};
+					alert("Add User Successfull!");
+				})
+				.catch((err) => alert("Add User Failed!"));
+		} else {
+			$http
+				.put(`https://dummyjson.com/users/${$scope.userData.id}`, $scope.userData)
+				.then((res) => {
+					$scope.formData = {};
+					$scope.isAdd = true;
+					alert("Update User Successfull!");
+				})
+				.catch((err) => alert("Update User Failed!"));
+		}
 	};
+
 	$scope.deleteUser = function (id) {
 		$http
 			.delete(`https://dummyjson.com/users/${id}`)
 			.then((res) => alert("Delete User Successfull!"))
 			.catch((err) => alert("Delete User Failed!"));
+	};
+
+	$scope.resetForm = function () {
+		console.log("userForm: ", $scope.userForm);
+		$scope.formData = {};
+		$scope.isAdd = true;
 	};
 
 	$scope.getUsers();
